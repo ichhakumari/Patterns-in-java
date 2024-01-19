76288e4ef9a957bd34c7fe0a478d82a87b47026b
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-01-19 20:49:49 +0530

    Add files via upload

---
+++ hallow_rect.java
@@ -0,0 +1,20 @@
+//print hallow rectangular pattern in java ...
+public class hallow_rect {
+    public static void main(String args[]) {
+        int n = 4;
+        int m = 8;
+        // outer loop
+        for (int i = 1; i <= n; i++) {
+            // inner loop
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
