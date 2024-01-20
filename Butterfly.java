afedfa04bcbeb7786db4617b12ecc49617a7b358
Ichha kumari <143002729+ichhakumari@users.noreply.github.com>
2024-01-20 22:49:15 +0530

    Add files via upload

---
+++ Butterfly.java
@@ -0,0 +1,45 @@
+//print butterfly pattern...
+public class Butterfly {
+    public static void main(String[] args) {
+        int n = 5;
+
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            // 1st part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+
+            // space
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        // lower half
+        for (int i = n; i >= 1; i--) {
+            // 1st part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+
+            // space
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
+}
